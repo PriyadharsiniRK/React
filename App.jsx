bc35196e636b509a54a0e032f68b0a8479dd03f2
Priyadharsini RK <156496813+PriyadharsiniRK@users.noreply.github.com>
2026-06-01 17:27:17 +0200

    Create App component for Spread operator

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import Spread from "./Spread.jsx"
+function App() {
+  return (
+      <div>
+        <h1>Interest Calculator</h1>
+
+        
+          <Spread/>
+      </div>
+  );
+}
+
+export default App;
