2ca09b7bec5971766e5132242774dea22d2c2bac
Priyadharsini RK <156496813+PriyadharsiniRK@users.noreply.github.com>
2026-06-02 20:11:34 +0200

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,12 +1,39 @@
-
+import { useState } from "react";
 import Spread from "./Spread.jsx"
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
 function App() {
+    
+  const loanAmount = 2500000;
+
+  const [interestRate, setInterestRate] = useState(0.03);
+
+  const interestCost = loanAmount * interestRate;
+
   return (
       <div>
         <h1>Interest Calculator</h1>
 
-        
+        <p>Loan Amount: {loanAmount}</p>
+
+        <button onClick={() => setInterestRate(0.03)}>
+          3%
+        </button>
+
+        <button onClick={() => setInterestRate(0.04)}>
+          4%
+        </button>
+
+        <button onClick={() => setInterestRate(0.05)}>
+          5%
+        </button>
+
+        <h2>Selected Rate: {interestRate * 100}%</h2>
+
+        <h2>Interest Cost: {interestCost}</h2>
+          <Header/>
           <Spread/>
+          <Footer/>
       </div>
   );
 }
---
+++ Header.jsx
@@ -0,0 +1,6 @@
+﻿function Header(){
+    return(
+        <h4>Welcome</h4>
+    );
+}
+export default Header;
